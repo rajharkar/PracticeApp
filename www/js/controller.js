2f43d92c318cef2dda5f2719b6836203576c4362
shravya harkar <shravya harkar>
2017-04-29 02:18:50 -0400

    FireBase Auth

--- www/js/controller.js
+++ www/js/controller.js
@@ -21,13 +21,56 @@ app.controller('GeoCtrl', function($firebaseObject,$scope) {
         $scope.leftButton = function(){
                   doSomething();
         }
-  //var posOptions = {timeout: 10000, enableHighAccuracy: false};
-  /*$cordovaGeolocation
-    .getCurrentPosition(posOptions)
-    .then(function (position)*/ {
-   /*   var lat  = position.coords.latitude
-      var long = position.coords.longitude*/
-    };
+
+
+      //get Elemets 
+      const txtEmail = document.getElementById('txtEmail');
+      const txtPassword = document.getElementById('txtPassword');
+      const btnLogin = document.getElementById('btnLogin');
+      const btnSignUp = document.getElementById('btnSignUp');
+     const btnLogout = document.getElementById('btnLogout');
+
+    
+       btnLogin.addEventListener('click' , e => {
+
+       const email = txtEmail.value;
+       const pass = txtPassword.value;
+       const auth = firebase.auth();
+       const promise= auth.signInWithEmailAndPassword(email,pass);
+       promise.catch(e=>console.log(e.message));
+       });
+
+
+      btnSignUp.addEventListener('click' , e => {
+
+       const email = txtEmail.value;
+       const pass = txtPassword.value;
+       const auth = firebase.auth();
+       const promise = auth.createUserWithEmailAndPassword(email,pass);
+       promise.catch(e=>console.log(e.message));
+       });
+
+
+
+      btnLogin.addEventListener('click' , e => {
+
+         firebase.auth().signOut();
+       });
+
+      firebase.auth().onAuthStateChanged(firebaseUser =>{
+        if(firebaseUser)
+        {
+        console.log(firebaseUser);
+        btnLogout.classList.remove('hide');
+      }
+        else 
+        {
+         console.log("Not logged in");   
+           btnLogout.classList.remove('add');
+        }
+      });
+
+  
 });
 
 function doSomething()
